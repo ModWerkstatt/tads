62b4512ac996d05c5bcc6b8a06502507aabed172
Florian Thiel <flth@flth.de>
2023-11-11 20:28:22 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,26 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "deutschland", "germany", "db", "waggon", "goods", },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+
+		params = {
+        },
+	},
+	options = {
+	},
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,58 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Grain cars DB Tads 958 and Tds 930",
+		["mod_desc"] = "In 1976 the Deutsche Bundesbahn 1500 car was built with a one-sided swivel roof. The wagons that were reserved for transporting food were labeled \"Getreide\" [grain]. The wagons are still in use at DB AG today. In 2021, some wagons were sold to EBS.",
+
+        ["tads_menu"] = "Tads 958 Swivel roof wagon",
+		["tads_db_name"] = "Tadgs-y 958 DB (1976)",
+		["tads_db2_name"] = "Tads-y 958 DB (1976)",
+		["tads_dbag_name"] = "Tadgs-y 958 DB AG (1994)",
+		["tads_dbag2_name"] = "Tads 958 DB AG (1994)",
+		["tads_dbag3_name"] = "Tadgs 958 DB AG (2000)",
+		["tads_ebs_name"] = "Tadgs-y 958 EBS (2021)",
+        ["sgns_desc"] = "In 1976 the DB 1500 car was built with a one-sided swivel roof. The wagons that were reserved for transporting food were labeled (Grain). The wagons are still in use at DB AG today. In 2021, some wagons were sold to EBS.",
+
+		["tds_menu"] = "Tds 930 Swivel roof wagon",
+		["tds_db_name"] = "Ktmmvs 69 DB (1962)",
+		["tds_db2_name"] = "Tdgs 930 DB (1965)",
+		["tds_db3_name"] = "Tds 930 DB (1965)",
+		["tds_dbag_name"] = "Tdgs-z 930 DB AG (1994)",
+		["tds_dbag2_name"] = "Tdgs-z 930 DB AG (1994)",
+		["tds_dbag3_name"] = "Tds-v 930 DB AG (2003)",
+		["tds_dbag4_name"] = "Tds 930 DB AG (2003)",
+		["tds_desc"] = "Parallel to the Otmm 70 is the DB 1965 building wagons with a swivel roof. This barely contained the Otmm 70, only the upper box was changed and a swivel roof was added. The wagon were used to transport food.",
+
+		["option_fake_wagen_desc"] = "Should the fake versions of the Tads and Tds be available in the game?",
+		["fake_tads_wagen"] = "Activate fake versions?",
+
+	},
+	de = {
+
+		["mod_name"] = "Getreidewagen Tads 958 und Tds 930",
+		["mod_desc"] = "1976 lies die Deutsche Bundesbahn 1500 Wagen mit einem einseitigen Schwenkdach bauen. Die Wagen, die für den Lebensmitteltransport reserviert waren, trugen die Aufschrift \"Getreide\". Die Wagen sind bis heute bei der DB AG im Einsatz. 2021 wurden einige Wagen an die EBS verkauft.",
+
+        ["tads_menu"] = "Tads 958 Schwenkdachwagen",
+		["tads_db_name"] = "Tadgs-y 958 DB (1976)",
+		["tads_db2_name"] = "Tads-y 958 DB (1976)",
+		["tads_dbag_name"] = "Tadgs-y 958 DB AG (1994)",
+		["tads_dbag2_name"] = "Tads 958 DB AG (1994)",
+		["tads_dbag3_name"] = "Tadgs 958 DB AG (2000)",
+		["tads_ebs_name"] = "Tadgs-y 958 EBS (2021)",
+        ["tads_desc"] = "1976 lies die DB 1500 Wagen mit einem Einseitigen Schwenkdach bauen. Die Wagen wo für den Lebensmitteltransport reserviert waren trugen die Aufschrift (Getreide). Die Wagen sind bis heute bei der DB AG im Einsatz 2021 wurden einige Wagen an die EBS verkauft.",
+
+		["tds_menu"] = "Tds 930 Schwenkdachwagen",
+		["tds_db_name"] = "Ktmmvs 69 DB (1962)",
+		["tds_db2_name"] = "Tdgs 930 DB (1965)",
+		["tds_db3_name"] = "Tds 930 DB (1965)",
+		["tds_dbag_name"] = "Tdgs-z 930 DB AG (1994)",
+		["tds_dbag2_name"] = "Tdgs-z 930 DB AG (1994)",
+		["tds_dbag3_name"] = "Tds-v 930 DB AG (2003)",
+		["tds_dbag4_name"] = "Tds 930 DB AG (2003)",
+		["tds_desc"] = "Parallel zum Otmm 70 lies die DB 1965 Wagen mit Schwenkdach bauen. Dieses unterschieden sich kaum zum Otmm 70, es wurde lediglich der Oberkasten verändert und ein Schwenkdach angebaut. Die Wagen wurden im Lebensmitteltransport eingesetzt.",
+		["fake_tads_wagen"] = "Fakeversionen aktivieren?",
+
+	},
+}
+end
